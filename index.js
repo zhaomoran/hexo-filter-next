f16d7ac17b751f61022bade8a4b1ed7d9a583b05
mrzhao <mrzhao@iflytek.com>
2021-07-28 12:51:23 +0800

    support mermaid

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 hexo.extend.filter.register('before_post_render', function (data) {
     if (data.source.endsWith("md") && data.content.indexOf("```mermaid") !== -1) {
-        data.content = data.content.replace(/(```mermaid)[\s]*(.*)(;)([\s\S]*?)(```)/g, '{% mermaid $2 %}$4{% endmermaid %}');
+        data.content = data.content.replace(/(```mermaid)[\s]*(.*)([\s\S]*?)(```)/g, '{% mermaid $2 %}$3{% endmermaid %}');
     }
 }, 9);
